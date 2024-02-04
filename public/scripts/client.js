7eb06c2511e0f2d25dc82d053a68b6c197536086
vagrant <pihugupta0501@gmail.com>
2024-02-04 02:05:11 +0000

    Implement the renderTweets function to loop through the array of tweets and display them dynamically from the defined tweets database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,42 +16,51 @@ $(document).ready(() => {
   //   }
   // });
 
-  const tweetdb = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1706825000175
-  }
+  const $tweetContainer = $('#tweet-container');
 
-  const createTweet = () => {
+  const tweets = [ 
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac"
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants"
+      },
+      created_at: 1706825000175
+    },
+
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd" },
+      content: {
+        text: "Je pense , donc je suis"
+      },
+      created_at: 1461113959088
+    }
+
+  ];
+
+  const createTweet = (tweet) => {
     const $tweet = $(`
-
     <article class="tweet">
       <header>
         <div class="user">
           <div class="profile">
-            <img src=${tweetdb.user.avatars}>
-            <h3>${tweetdb.user.name}</h3>
+            <img src=${tweet.user.avatars}>
+            <h3>${tweet.user.name}</h3>
           </div>
-          <h3>${tweetdb.user.handle}</h3>
+          <h3>${tweet.user.handle}</h3>
         </div>
         <div>
-          <p>
-          ${tweetdb.content.text}
-          </p>
+          <p>${tweet.content.text}</p>
         </div>
       </header>
-            
       <footer>
-
-        <div>
-          <h4>10 days ago</h4>
-        </div>
+        <div> <h4>10 days ago</h4></div>
               
         <div class="tweet-icons">
           <i class="fa-solid fa-flag"></i>
@@ -63,11 +72,19 @@ $(document).ready(() => {
   `);
     return $tweet;
   };
-  
-  const $tweet = createTweet();
-  const $tweetContainer = $('#tweet-container');
 
-  $tweetContainer.prepend($tweet);
+  const renderTweet = (tweets) => {
+    for (const tweet of tweets) {
+      const $tweet = createTweet(tweet);
+      $tweetContainer.prepend($tweet);
+    }
+  };
+  
+  renderTweet(tweets);
+
+  
+
+  
   console.log('$tweetContainer', $tweetContainer);
 
 });
