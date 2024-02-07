8b277b427e2eeb3efae80fcfdadfa91e5a454e6f
vagrant <pihugupta0501@gmail.com>
2024-02-07 21:21:14 +0000

    Clean up the code using prettier and delete unnecesary code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,16 +4,12 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
 $(document).ready(() => {
+  const $tweetContainer = $("#tweet-container");
+  const $newTweetForm = $("#new-tweet-form");
 
-  const $tweetContainer = $('#tweet-container');
-  const $newTweetForm = $('#new-tweet-form');
-  
-  
-  const $error = $('#error');
-  const $errorText = $('#error-text');
+  const $error = $("#error");
+  const $errorText = $("#error-text");
 
   //Cross-site scripting
   const escape = function (str) {
@@ -22,53 +18,16 @@ $(document).ready(() => {
     return div.innerHTML;
   };
 
-  const tweets = [ 
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac"
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants"
-      },
-      created_at: 1706825000175
-    },
-
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd" },
-      content: {
-        text: "Je pense , donc je suis"
-      },
-      created_at: 1461113959088
-    },
-
-    {
-      user: {
-        name: "Einstein",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd" },
-      content: {
-        text: "Je pense , donc je suis"
-      },
-      created_at: 1461113959088
-    }
-
-  ];
-
   const createTweet = (tweet) => {
     const $tweet = $(`
     <article class="tweet">
       <header>
         <div class="user">
           <div class="profile">
-            <img src=${escape(tweet.user.avatars)}>
-            <h3>${escape(tweet.user.name)}</h3>
+            <img src=${tweet.user.avatars}>
+            <h3>${tweet.user.name}</h3>
           </div>
-          <h3>${escape(tweet.user.handle)}</h3>
+          <h3>${tweet.user.handle}</h3>
         </div>
         <div>
           <p>${escape(tweet.content.text)}</p>
@@ -91,7 +50,6 @@ $(document).ready(() => {
   const renderTweet = (tweets) => {
     $tweetContainer.empty();
 
-    
     for (const tweet of tweets) {
       const $tweet = createTweet(tweet);
 
@@ -101,12 +59,12 @@ $(document).ready(() => {
 
   const loadTweets = () => {
     $.ajax({
-      url: '/tweets',
-      method: 'GET',
+      url: "/tweets",
+      method: "GET",
       success: (tweets) => {
         console.log(tweets);
         renderTweet(tweets);
-      }
+      },
     });
   };
 
@@ -115,39 +73,30 @@ $(document).ready(() => {
   //Ensure the tweets already in the databsse load as soon as the page loads
   loadTweets();
 
-  $newTweetForm.on('submit', (event) => {
+  $newTweetForm.on("submit", (event) => {
     event.preventDefault();
 
     //Form validation
-    const $tweetText = $('#tweet-text').val();
-    
+    const $tweetText = $("#tweet-text").val();
 
     if (!$tweetText) {
       $error.show();
-      $errorText.text('Your tweet is empty');
-
+      $errorText.text("Your tweet is empty");
 
       setTimeout(() => {
         $error.hide();
-    }, 3000); // Hide the error message after 3 seconds
-
-
-    } else if (($tweetText.length + 1) > 140) {
+      }, 3000); // Hide the error message after 3 seconds
+    } else if ($tweetText.length + 1 > 140) {
       $error.show();
-      $errorText.text('Your tweet is too long');
-
+      $errorText.text("Your tweet is too long");
 
       setTimeout(() => {
         $error.hide();
-    }, 3000); // Hide the error message after 3 seconds
-
-
-    } else  {
+      }, 3000); // Hide the error message after 3 seconds
+    } else {
       //serialize the form data
       const data = $newTweetForm.serialize();
-      console.log('hello form');
-
-      
+      console.log("hello form");
 
       //Post the data
       $.ajax({
@@ -156,22 +105,17 @@ $(document).ready(() => {
         data: data,
         success: (response) => {
           console.log(response);
-          $('#tweet-text').val(''); // Clear the input field
+          $("#tweet-text").val(""); // Clear the input field
           $(".counter").text(140); //reset the tweet counter
           loadTweets();
         },
         error: (error) => {
           console.log(error);
-        }
+        },
       });
-
-    };
-
+    }
   });
 
   $error.hide();
-  $errorText.text('');
-
-  
-
+  $errorText.text("");
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,12 +3,11 @@ const charCounter = function (text) {
   return count;
 };
 
-
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
 
-  $( "#tweet-text" ).on( " input keypress", function() {
-    const tweetText = $( "#tweet-text" ).val();
+  $("#tweet-text").on(" input keypress", function () {
+    const tweetText = $("#tweet-text").val();
     const tweetTextCount = charCounter(tweetText);
 
     // console.log(tweetTextCount);
@@ -19,15 +18,10 @@ $(document).ready(function() {
 
     if (tweetTextCount > 140) {
       // console.log('output is working!');
-      $("output").addClass('red-char')
+      $("output").addClass("red-char");
     } else {
       // console.log('output is working else!');
-      $("output").removeClass('red-char')
-    }  
-  } );
-
-
-  
-
+      $("output").removeClass("red-char");
+    }
+  });
 });
-
